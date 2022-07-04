cdc7dbcfdbfbe64c8b81dbdda3e5b2f22d5e4685
Montse <m.barrionuevo1@gmail.com>
2022-07-04 21:54:34 +0200

    Rutas primeros pasos 01

---
+++ src/Controller/PreguntaController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PreguntaController
+{
+    /**
+     * @Route("/")
+     */
+    public function home(){
+        return new Response( content: 'Bienvenido a la portada!');
+    }
+
+    /**
+     * @Route("/preguntas/{slug}")
+     */
+    public function mostrar($slug){
+        return new Response( sprintf('Hola %s', $slug));
+    }
+}
